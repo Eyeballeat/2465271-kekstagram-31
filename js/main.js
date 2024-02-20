620d83597a913a77974ef258d36b1db0d61da5a3
Eyeballeat <eyeballeat@gmail.com>
2024-02-20 14:34:22 +0200

    Задание 4.16 с переработанной функцией

--- js/main.js
+++ js/main.js
@@ -2,9 +2,6 @@
 const AUTORS = ['Артем', 'Андрей', 'Сергей', 'Владимир', 'Дмитрий', 'Василий', 'Александр', 'Вячеслав', 'Антон', 'Евгений', 'Михаил', 'Денис', 'Мария', 'Татьяна', 'Елена', 'Анастасия', 'Виктория', 'Екатерина', 'Светлана', 'Юлия', 'Юрий', 'Валерия', 'Станислав', 'Ольга', 'Любовь'];
 const DESCRIPTIONS = ['Красота!', 'Бывает же такое!', 'Классное фото!', 'Обожаю!', 'Без комментариев', 'Лучше промолчать!', 'Комментарии здесь излишни!', 'Очень интересная фотография!'];
 const COMMENT_MESSAGES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.', 'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.', 'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.', 'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.', 'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
-const SIMILAR_OBJECT_COUNT = 25;
-const COMMENTS_COUNT = 30;
-const MESSAGE_ID_COUNT = SIMILAR_OBJECT_COUNT * COMMENTS_COUNT;
 // Создаем функцию возвращающую рандомное число
 const getRandomData = (min, max) => {
   const lower = Math.ceil(Math.min(min, max));
@@ -29,25 +26,26 @@ function createNoRepeatData (min, max) {
 }
 // Создаем функциювозвращающую случайный эемент массива
 const getRandomArrayElement = (elements) => elements[getRandomData(0, elements.length - 1)];
-// Создаем не повторяющиеся случайные значения ключей
-const generateDescriptionID = createNoRepeatData(1, SIMILAR_OBJECT_COUNT);
-const generateDescriptionURL = createNoRepeatData(1, SIMILAR_OBJECT_COUNT);
-const generateCommentID = createNoRepeatData(1, MESSAGE_ID_COUNT);
-// Создаем массив объектов comments
-const createCommentsDescription = () => ({
-  id: generateCommentID(),
-  avatar: `img/avatar-${getRandomData(1, 6)}`,
-  message: `${getRandomArrayElement(COMMENT_MESSAGES)}`,
-  name: `${getRandomArrayElement(AUTORS)}`
-});
-// Создаем массив объектов согласно ТЗ
-const createPhotoDescription = () => ({
-  id: generateDescriptionID(),
-  url: `photos/${generateDescriptionURL()}.jpg`,
-  description: `${getRandomArrayElement(DESCRIPTIONS)}`,
-  likes: getRandomData(15,200),
-  comments: Array.from({length: getRandomData(0, COMMENTS_COUNT)}, createCommentsDescription)
-});
-// Создаем результирующий массив
-const resultDescriptions = () => Array.from({length: SIMILAR_OBJECT_COUNT}, createPhotoDescription);
-console.log(resultDescriptions());
+// Создаем функцию возвращающую результат
+const getResultObject = (objectCount, commentCount) => {
+  const generateDescriptionID = createNoRepeatData(1, objectCount);
+  const generateDescriptionURL = createNoRepeatData(1, objectCount);
+  const generateCommentID = createNoRepeatData(1, objectCount * commentCount);
+
+  const createCommentsDescription = () => ({
+    id: generateCommentID(),
+    avatar: `img/avatar-${getRandomData(1, 6)}`,
+    message: `${getRandomArrayElement(COMMENT_MESSAGES)}`,
+    name: `${getRandomArrayElement(AUTORS)}`
+  });
+
+  const createPhotoDescription = () => ({
+    id: generateDescriptionID(),
+    url: `photos/${generateDescriptionURL()}.jpg`,
+    description: `${getRandomArrayElement(DESCRIPTIONS)}`,
+    likes: getRandomData(15,200),
+    comments: Array.from({length: getRandomData(0, commentCount)}, createCommentsDescription)
+  });
+  return Array.from({length: objectCount}, createPhotoDescription);
+};
+console.log(getResultObject(25,30));
